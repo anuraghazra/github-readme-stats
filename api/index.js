a9c8394ce06e31b12a0d89346481110d407b2bc3
Manolo Edge <nombrekeff@rallf.com>
2020-07-14 21:57:51 +0200

    Changed cache control directives to public, max-age

--- api/index.js
+++ api/index.js
@@ -189,7 +189,7 @@ module.exports = async (req, res) => {
 
   const stats = await fetchStats(username);
 
-  res.setHeader('Cache-Control', 's-maxage=300');
+  res.setHeader('Cache-Control', 'public, max-age=300');
   res.setHeader("Content-Type", "image/svg+xml");
   res.send(
     renderSVG(stats, {
