6d5a8285429b2b4e4f6ba00d9a5a5d8bb49b4d72
anuraghazra <hazru.anurag@gmail.com>
2020-07-25 13:29:32 +0530

    chore: added secondary warning message in renderError

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,20 @@
 const axios = require("axios");
 const themes = require("../themes");
 
-const renderError = (message) => {
+const renderError = (message, secondaryMessage = "") => {
   return `
-    <svg width="495" height="100" viewBox="0 0 495 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="495" height="120" viewBox="0 0 495 120" fill="none" xmlns="http://www.w3.org/2000/svg">
     <style>
     .text { font: 600 16px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2F80ED }
     .small { font: 600 12px 'Segoe UI', Ubuntu, Sans-Serif; fill: #252525 }
+    .gray { fill: #858585 }
     </style>
     <rect x="0.5" y="0.5" width="494" height="99%" rx="4.5" fill="#FFFEFE" stroke="#E4E2E2"/>
     <text x="25" y="45" class="text">Something went wrong! file an issue at https://git.io/JJmN9</text>
-    <text id="message" x="25" y="65" class="text small">${message}</text>
+    <text data-testid="message" x="25" y="55" class="text small">
+      <tspan x="25" dy="18">${message}</tspan>
+      <tspan x="25" dy="18"  class="gray">${secondaryMessage}</tspan>
+    </text>
     </svg>
   `;
 };
