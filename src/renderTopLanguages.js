be8574c150ab8745b60523b7d035249357d8bf6e
Liam <terroridk@gmail.com>
2020-07-26 18:23:02 -0400

    properly merge and test

--- src/renderTopLanguages.js
+++ src/renderTopLanguages.js
@@ -80,11 +80,17 @@ const renderTopLanguages = (topLangs, options = {}) => {
         .lang-name { font: 400 11px 'Segoe UI', Ubuntu, Sans-Serif; fill: ${textColor} }
       </style>
       
-      ${
-        hide_border
-          ? `<rect data-testid="card-bg" x="0.5" y="0.5" width="99.7%" height="99%" rx="4.5" fill="${bgColor}" stroke="#E4E2E2" stroke-opacity="0"/>`
-          : `<rect data-testid="card-bg" x="0.5" y="0.5" width="99.7%" height="99%" rx="4.5" fill="${bgColor}" stroke="#E4E2E2" stroke-opacity="1"/>`
-      }
+      <rect
+        data-testid="card-bg"
+        x="0.5"
+        y="0.5"
+        width="99.7%"
+        height="99%"
+        rx="4.5"
+        fill="${bgColor}" 
+        stroke="#E4E2E2"
+        stroke-opacity="${hide_border ? 0 : 1}"
+      />
       
       ${
         hide_title
