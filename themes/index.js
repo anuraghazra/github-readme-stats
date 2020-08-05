8acd429d448c82e9c4946a294209ca70418155af
IK <36619465+kazukazuinaina@users.noreply.github.com>
2020-08-06 01:35:08 +0900

    style(theme): Add gotham theme (#317)
    * [add] gotham-theme
    
    * fix gotham colorscheme

--- themes/index.js
+++ themes/index.js
@@ -149,6 +149,12 @@ const themes = {
     icon_color: "88c0d0",
     bg_color: "2e3440",
   },
+  "gotham": {
+    title_color: "2aa889",
+    icon_color: "599cab",
+    text_color: "99d1ce",
+    bg_color: "0c1014",
+  },
 };
 
 module.exports = themes;
