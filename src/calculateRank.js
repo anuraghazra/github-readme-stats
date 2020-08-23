df535c8c99086f1ebbb03fea91052faa0556d1a1
romadryud <romadryud@gmail.com>
2020-08-23 19:35:01 +0200

    refactoring CalculateRank

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -1,15 +1,15 @@
 // https://stackoverflow.com/a/5263759/10629172
 function normalcdf(mean, sigma, to) {
-  var z = (to - mean) / Math.sqrt(2 * sigma * sigma);
-  var t = 1 / (1 + 0.3275911 * Math.abs(z));
-  var a1 = 0.254829592;
-  var a2 = -0.284496736;
-  var a3 = 1.421413741;
-  var a4 = -1.453152027;
-  var a5 = 1.061405429;
-  var erf =
+  let z = (to - mean) / Math.sqrt(2 * sigma * sigma);
+  let t = 1 / (1 + 0.3275911 * Math.abs(z));
+  let a1 = 0.254829592;
+  let a2 = -0.284496736;
+  let a3 = 1.421413741;
+  let a4 = -1.453152027;
+  let a5 = 1.061405429;
+  let erf =
     1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
-  var sign = 1;
+  let sign = 1;
   if (z < 0) {
     sign = -1;
   }
@@ -57,34 +57,33 @@ function calculateRank({
     issues * ISSUES_OFFSET +
     stargazers * STARS_OFFSET +
     prs * PRS_OFFSET +
-    followers * FOLLOWERS_OFFSET + 
-    totalRepos * REPO_OFFSET 
+    followers * FOLLOWERS_OFFSET +
+    totalRepos * REPO_OFFSET
   ) / 100;
 
   const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
 
   let level = "";
 
-  if (normalizedScore < RANK_S_VALUE) {
-    level = "S+";
-  }
-  if (
-    normalizedScore >= RANK_S_VALUE &&
-    normalizedScore < RANK_DOUBLE_A_VALUE
-  ) {
-    level = "S";
-  }
-  if (
-    normalizedScore >= RANK_DOUBLE_A_VALUE &&
-    normalizedScore < RANK_A2_VALUE
-  ) {
-    level = "A++";
-  }
-  if (normalizedScore >= RANK_A2_VALUE && normalizedScore < RANK_A3_VALUE) {
-    level = "A+";
-  }
-  if (normalizedScore >= RANK_A3_VALUE && normalizedScore < RANK_B_VALUE) {
-    level = "B+";
+  switch (normalizedScore) {
+    case normalizedScore < RANK_S_VALUE:
+      level = "S+";
+      break;
+    case normalizedScore >= RANK_S_VALUE && normalizedScore < RANK_DOUBLE_A_VALUE:
+      level = "S";
+      break;
+    case normalizedScore >= RANK_DOUBLE_A_VALUE && normalizedScore < RANK_A2_VALUE:
+      level = "A++";
+      break;
+    case normalizedScore >= RANK_A2_VALUE && normalizedScore < RANK_A3_VALUE:
+      level = "A+";
+      break;
+    case normalizedScore >= RANK_A3_VALUE && normalizedScore < RANK_B_VALUE:
+      level = "B+";
+      break;
+    default:
+      level = "";
+      break;
   }
 
   return { level, score: normalizedScore };
