e3d6f9503123cafb4b1084bc859800967197bb64
Matheus Gonçalves <mgmadvance@hotmail.com>
2020-08-27 11:04:35 -0300

    style(theme): Add Omni theme (#424)
    Co-authored-by: Anurag Hazra <hazru.anurag@gmail.com>

--- themes/index.js
+++ themes/index.js
@@ -209,6 +209,12 @@ const themes = {
     text_color: "ebcfb2",
     bg_color: "373f51",
   },
+  omni: {
+    title_color: "FF79C6",
+    icon_color: "e7de79",
+    text_color: "E1E1E6",
+    bg_color: "191622"
+  },
   react: {
     title_color: "61dafb",
     icon_color: "61dafb",
