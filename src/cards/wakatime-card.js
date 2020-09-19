e8de1fc5498dd1f621ff62e9e6a5bbb84c48d159
Max Niederman <max@maxniederman.com>
2020-09-19 16:19:28 -0700

    fix: use consistent case for card titles
    The WakaTIme stats card used different casing for the title than other cards.

--- src/cards/wakatime-card.js
+++ src/cards/wakatime-card.js
@@ -120,7 +120,7 @@ const renderWakatimeCard = (stats = {}, options = { hide: [] }) => {
   });
 
   const card = new Card({
-    title: "Wakatime week stats",
+    title: "Wakatime Week Stats",
     width: 495,
     height,
     colors: {
