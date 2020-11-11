aac1df8a7bcec1cd07cfb49bb7e23dbbfd61bc49
Wattanai Sathuphan <wattanai.sathuphan@gmail.com>
2020-11-12 02:00:27 +0700

    PH theme :sweat_drops:

--- themes/index.js
+++ themes/index.js
@@ -262,7 +262,13 @@ const themes = {
     icon_color: "a64833",
     text_color: "d9c8a9",
     bg_color: "402b23"
-  }
+  },
+  pornhub: {
+    title_color: "ffa31a",
+    icon_color: "ffffff",
+    text_color: "ffffff",
+    bg_color: "000000"
+  },
 };
 
 module.exports = themes;
