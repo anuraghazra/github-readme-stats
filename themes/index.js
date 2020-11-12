9931414f1af2c597b6460b10b05353e7e8efaea5
Avanti Bhandarkar <48968274+avanti-bhandarkar@users.noreply.github.com>
2020-11-12 14:20:53 +0530

    Update index.js
    added Nord theme

--- themes/index.js
+++ themes/index.js
@@ -262,6 +262,19 @@ const themes = {
     icon_color: "a64833",
     text_color: "d9c8a9",
     bg_color: "402b23"
+  },
+    nord_dark: {
+    title_color: "8fbcbb",
+    icon_color: "e5e9f0",
+    text_color: "eceff4",
+    bg_color: "2E3440"
+  }
+  ,
+    nord: {
+    title_color: "8fbcbb",
+    icon_color: "e5e9f0",
+    text_color: "eceff4",
+    bg_color: "2E3440"
   }
 };
 
