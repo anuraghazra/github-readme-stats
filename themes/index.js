f3248c7d8c52de1efee1e1ec9a5e171bced2cbdb
sp1rit <sp1rit@disroot.org>
2020-12-12 23:12:13 +0100

    style(theme): added nord theme

--- themes/index.js
+++ themes/index.js
@@ -262,6 +262,12 @@ const themes = {
     icon_color: "a64833",
     text_color: "d9c8a9",
     bg_color: "402b23"
+  },
+  nord: {
+    title_color: "88c0d0",
+    icon_color: "8fbcbb",
+    text_color: "eceff4",
+    bg_color: "2e3440"
   }
 };
 
