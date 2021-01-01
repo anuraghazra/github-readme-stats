59026c5770f3974500449b997145b471308572b5
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-12-31 23:46:26 -0300

    RedTouch-Light

--- themes/index.js
+++ themes/index.js
@@ -262,7 +262,13 @@ const themes = {
     icon_color: "a64833",
     text_color: "d9c8a9",
     bg_color: "402b23"
-  }
+  },
+    "redtouch-light": {
+      title_color: "ff1744",
+      icon_color: "f05545",
+      text_color: "333333",
+      bg_color: "fffefe"
+   },
 };
 
 module.exports = themes;
