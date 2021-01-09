c8c3a5090ee28db9348056d0d3f12d173414cf0a
カワリミ人形 <kawarimidoll+git@gmail.com>
2021-01-09 11:27:51 +0900

    refactor calculateRank.js
    use const instead of var

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -1,19 +1,16 @@
 // https://stackoverflow.com/a/5263759/10629172
 function normalcdf(mean, sigma, to) {
-  var z = (to - mean) / Math.sqrt(2 * sigma * sigma);
-  var t = 1 / (1 + 0.3275911 * Math.abs(z));
-  var a1 = 0.254829592;
-  var a2 = -0.284496736;
-  var a3 = 1.421413741;
-  var a4 = -1.453152027;
-  var a5 = 1.061405429;
-  var erf =
+  const z = (to - mean) / Math.sqrt(2 * sigma * sigma);
+  const t = 1 / (1 + 0.3275911 * Math.abs(z));
+  const a1 = 0.254829592;
+  const a2 = -0.284496736;
+  const a3 = 1.421413741;
+  const a4 = -1.453152027;
+  const a5 = 1.061405429;
+  const erf =
     1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
-  var sign = 1;
-  if (z < 0) {
-    sign = -1;
-  }
-  return (1 / 2) * (1 + sign * erf);
+  const sign = z >= 0 ? 1 : -1;
+  return (1 + sign * erf) / 2;
 }
 
 function calculateRank({
