da5b487992d7258391280841ceb9f99e5f96386d
Mitchell Skaggs <skaggsm333@gmail.com>
2021-01-16 09:55:03 -0600

    Use exact normal CDF function for rank calculation
    Uses Math.js for erf function

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -1,19 +1,9 @@
-// https://stackoverflow.com/a/5263759/10629172
+const { erf } = require("mathjs");
+const sqrt2 = Math.sqrt(2);
 function normalcdf(mean, sigma, to) {
-  var z = (to - mean) / Math.sqrt(2 * sigma * sigma);
-  var t = 1 / (1 + 0.3275911 * Math.abs(z));
-  var a1 = 0.254829592;
-  var a2 = -0.284496736;
-  var a3 = 1.421413741;
-  var a4 = -1.453152027;
-  var a5 = 1.061405429;
-  var erf =
-    1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
-  var sign = 1;
-  if (z < 0) {
-    sign = -1;
-  }
-  return (1 / 2) * (1 + sign * erf);
+  const z = (to - mean) / (sigma * sqrt2);
+  const erfZ = erf(z);
+  return 0.5 + 0.5 * erfZ;
 }
 
 function calculateRank({
@@ -57,8 +47,8 @@ function calculateRank({
     issues * ISSUES_OFFSET +
     stargazers * STARS_OFFSET +
     prs * PRS_OFFSET +
-    followers * FOLLOWERS_OFFSET + 
-    totalRepos * REPO_OFFSET 
+    followers * FOLLOWERS_OFFSET +
+    totalRepos * REPO_OFFSET
   ) / 100;
 
   const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
