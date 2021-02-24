e4ab221b433d6858c74f3d080e8cc74ac212f84c
Vinayak Sharma <vinayak1998th@Gmail.com>
2021-02-24 22:39:03 +0530

    Added Ocean_dark Theme
    ocean_dark:{
        title_color: "fe5e5e",
        icon_color: "b08ecb",
        text_color: "c8e0ab",
        bg_color: "0e111a",
      }
    
    Based on material dark theme

--- themes/index.js
+++ themes/index.js
@@ -268,6 +268,12 @@ const themes = {
     icon_color: "ff1aff",
     text_color: "8080ff",
     bg_color: "141439",
+  },
+  ocean_dark:{
+    title_color: "fe5e5e",
+    icon_color: "b08ecb",
+    text_color: "c8e0ab",
+    bg_color: "0e111a",
   }
 };
 
