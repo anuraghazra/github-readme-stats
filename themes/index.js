73cb243005ce7404c1155c9bcd89e2f8cd359bc4
Raphael Marques <34493798+raphael-ms@users.noreply.github.com>
2021-03-02 09:04:32 -0300

    Update index.js
    Add brazil theme, respecting the HEX code of Brazilian flag

--- themes/index.js
+++ themes/index.js
@@ -268,6 +268,12 @@ const themes = {
     icon_color: "ff1aff",
     text_color: "8080ff",
     bg_color: "141439",
+  },
+  brazil:{
+   title_color: "ffdf00",
+   icon_color: "002776",
+   text_color: "ffdf00",
+   bg_color: "009c3b",
   }
 };
 
