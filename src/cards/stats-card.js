bee9ba7cf1eaefc84fd1e89d3b86421f95719673
Alexandr <qwerty541zxc@gmail.com>
2021-03-21 23:42:02 +0200

    removed redundant comparison

--- src/cards/stats-card.js
+++ src/cards/stats-card.js
@@ -130,7 +130,7 @@ const renderStatsCard = (stats = {}, options = { hide: [] }) => {
   };
 
   const longLocales = ["cn", "es", "fr", "pt-br", "ru", "uk-ua", "id", "my", "pl"];
-  const isLongLocale = longLocales.includes(locale) === true;
+  const isLongLocale = longLocales.includes(locale);
 
   // filter out hidden stats defined by user & create the text nodes
   const statItems = Object.keys(STATS)
