e765a4e76d892aedf8225ebd381c1fbb7a85a758
Nikolay Korolev <CrafterKolyan@mail.ru>
2021-03-22 20:11:32 +0300

    Update calculateRank.test.js

--- tests/calculateRank.test.js
+++ tests/calculateRank.test.js
@@ -15,4 +15,32 @@ describe("Test calculateRank", () => {
       }),
     ).toStrictEqual({ level: "A+", score: 46.164852607446775 });
   });
+  
+  it("should be able to get B+ rank", () => {
+    expect(
+      calculateRank({
+        totalCommits: 0,
+        totalRepos: 0,
+        followers: 0,
+        contributions: 0,
+        stargazers: 0,
+        prs: 0,
+        issues: 0,
+      }),
+    ).toStrictEqual({ level: "B+", score: 100 });
+  });
+  
+  it("should be able to get S+ rank for Linus Torvalds", () => {
+    expect(
+      calculateRank({
+        totalCommits: 2700,
+        totalRepos: 2,
+        followers: 132000,
+        contributions: 2700,
+        stargazers: 109100,
+        prs: 71,
+        issues: 2,
+      }),
+    ).toStrictEqual({ level: "S+", score: 50.069648962437896 });
+  });
 });
