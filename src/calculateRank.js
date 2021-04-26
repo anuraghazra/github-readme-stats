eb2fd9d8cada0687c62614f28565390b05d27159
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-04-26 14:53:23 -0500

    Undo code format made by previous commit

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -8,7 +8,7 @@ function normalcdf(mean, sigma, to) {
   var a4 = -1.453152027;
   var a5 = 1.061405429;
   var erf =
-      1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
+    1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
   var sign = 1;
   if (z < 0) {
     sign = -1;
@@ -33,8 +33,13 @@ function calculateRank({
   const FOLLOWERS_OFFSET = 0.45;
   const REPO_OFFSET = 1;
 
-  const ALL_OFFSETS = CONTRIBS_OFFSET + ISSUES_OFFSET + STARS_OFFSET +
-                      PRS_OFFSET + FOLLOWERS_OFFSET + REPO_OFFSET;
+  const ALL_OFFSETS =
+    CONTRIBS_OFFSET +
+    ISSUES_OFFSET +
+    STARS_OFFSET +
+    PRS_OFFSET +
+    FOLLOWERS_OFFSET +
+    REPO_OFFSET;
 
   const RANK_S_VALUE = 1;
   const RANK_DOUBLE_A_VALUE = 25;
@@ -43,14 +48,18 @@ function calculateRank({
   const RANK_B_VALUE = 100;
 
   const TOTAL_VALUES =
-      RANK_S_VALUE + RANK_A2_VALUE + RANK_A3_VALUE + RANK_B_VALUE;
+    RANK_S_VALUE + RANK_A2_VALUE + RANK_A3_VALUE + RANK_B_VALUE;
 
   // prettier-ignore
-  const score =
-      (totalCommits * COMMITS_OFFSET + contributions * CONTRIBS_OFFSET +
-       issues * ISSUES_OFFSET + stargazers * STARS_OFFSET + prs * PRS_OFFSET +
-       followers * FOLLOWERS_OFFSET + totalRepos * REPO_OFFSET) /
-      100;
+  const score = (
+    totalCommits * COMMITS_OFFSET +
+    contributions * CONTRIBS_OFFSET +
+    issues * ISSUES_OFFSET +
+    stargazers * STARS_OFFSET +
+    prs * PRS_OFFSET +
+    followers * FOLLOWERS_OFFSET + 
+    totalRepos * REPO_OFFSET 
+  ) / 100;
 
   const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
 
