9d53dc088891157bfd906b70cdfdbe18944489d1
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2021-05-31 11:01:09 +0200

    New contribution
     added Hulk color palette

--- themes/index.js
+++ themes/index.js
@@ -288,6 +288,12 @@ const themes = {
     text_color: "C3D1D9",
     bg_color: "0D1117",
   },
+  hulk: {
+    title_color: "#614196",
+    icon_color: "#614196",
+    text_color: "#F2F2F2",
+    bg_color: "097A3A",
+  },
 };
 
 module.exports = themes;
