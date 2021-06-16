63361ba793665f591e57b37c2b227bc57d8ad58d
LorisonGilles <54437398+Gilles30@users.noreply.github.com>
2021-06-16 20:22:23 -0300

    Create Ocean_dark

--- themes/index.js
+++ themes/index.js
@@ -294,6 +294,12 @@ const themes = {
     text_color: "FFFFFF",
     bg_color: "2C2F33",
   },
+  ocean_dark: {
+    title_color: "8957B2",
+    icon_color: "FFFFFF",
+    text_color: "92D534",
+    bg_color: "151A28",
+  },
 };
 
 module.exports = themes;
