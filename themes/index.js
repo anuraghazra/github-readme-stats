0f2e061e38c83dab7cf87c1c67bbf3f60eb66dc0
Eliarlan RM <87993239+eliarlanRM@users.noreply.github.com>
2021-08-13 19:45:00 -0300

    Created default_red

--- themes/index.js
+++ themes/index.js
@@ -6,6 +6,13 @@ const themes = {
     bg_color: "fffefe",
     border_color: "e4e2e2",
   },
+  default_red: {
+    title_color: "FF0000",
+    icon_color: "FF2800",
+    text_color: "333",
+    bg_color: "fffefe",
+    border_color: "e4e2e2",
+  },
   default_repocard: {
     title_color: "2f80ed",
     icon_color: "586069", // icon color is different
