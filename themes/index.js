49215985625747511f53ea55b2d6e007cebe679c
Leonardo Dognani <82122343+Leonardodognani@users.noreply.github.com>
2021-09-05 18:19:48 -0300

    New theme, dusk, to index.js
    Sending a new theme (again).
    Hope you like it =)
    ![dusk](https://user-images.githubusercontent.com/82122343/132141730-5042093c-9c84-464e-be66-a7467db6e27f.jpg)

--- themes/index.js
+++ themes/index.js
@@ -294,6 +294,12 @@ const themes = {
     text_color: "FFFFFF",
     bg_color: "2C2F33",
   },
+  dusk: {
+    title_color: "ff6000",
+    icon_color: "ffea00",
+    text_color: "ff6000",
+    bg_color: "000000",
+  },
 };
 
 module.exports = themes;
