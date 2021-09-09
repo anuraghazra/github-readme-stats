7b489e286d273211c7f5525f5f2dd7c3c6af7c0a
Neotecna <leonelbrito1996@outlook.com>
2021-09-09 17:49:02 -0400

    Updated Software (CodeTime) card

--- src/fetchers/codetime-fetcher.js
+++ src/fetchers/codetime-fetcher.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
-const fetchCodetimeStats = async ({username, api_domain, range}) => {
+const fetchCodetimeStats = async ({ username, api_domain, range }) => {
     try {
-        const {data} = await.axios.get(`https://${api_domain ? api_domain.replace(/\/$/gi, "") : "app.software.com"}/api/v1/users/${username}/stats/${range || ''}?is_including_today=true`);
+        const { data } = await axios.get(`https://${api_domain ? api_domain.replace(/\/$/gi, "") : "app.software.com"}/api/v1/users/${username}/stats/${range || ''}?is_including_today=true`);
 
         return data.data;
 
     } catch (err) {
-        if(err.response.status < 200 || err.response.status > 299){
+        if (err.response.status < 200 || err.response.status > 299) {
             throw new Error("CodeTime user not found, make sure you have a CodeTime profile");
         }
 
@@ -15,4 +15,4 @@ const fetchCodetimeStats = async ({username, api_domain, range}) => {
     }
 };
 
-module.exports = {fetchCodetimeStats};
+module.exports = { fetchCodetimeStats };
