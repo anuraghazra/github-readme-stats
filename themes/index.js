419dbe4e993d7a6e21097bb0828dd2075d277e27
Arun singh thakur <arunt7767@gmail.com>
2021-09-12 23:43:24 +0530

    add
    added a new theme
     name: "Neon Knights"
    ''' I'm just learning how to contribute to open source

--- themes/index.js
+++ themes/index.js
@@ -294,6 +294,12 @@ const themes = {
     text_color: "FFFFFF",
     bg_color: "2C2F33",
   },
+  Neon Knights : {
+    title_color: "00FF00",
+    icon_color: "00FF00",
+    text_color: "ffffff",
+    bg_color: "000000",
+  },
 };
 
 module.exports = themes;
