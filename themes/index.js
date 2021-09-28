913f9b4907274c7119347b6c4c9105902a80d7a8
saltwalks <ewqdsacxz2345@gmail.com>
2021-09-29 01:09:25 +0900

    [design] add theme : starrynight

--- themes/index.js
+++ themes/index.js
@@ -330,6 +330,12 @@ const themes = {
   text_color: "355E3B",
   bg_color: "8A9A5B",
 }, 
+starrynight: {
+  title_color: "FFFF8F",
+  icon_color: "FFFF8F",
+  text_color: "023020",
+  bg_color: "6082B6",
+}, 
 };
 
 module.exports = themes;
