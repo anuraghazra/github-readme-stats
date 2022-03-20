49654c314e457ad56ed114c6949d67f18c005963
Thiago Gentil <71569140+Thiagonox@users.noreply.github.com>
2022-03-20 14:23:07 -0300

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -361,6 +361,13 @@ const themes = {
     text_color: "e0def4",
     bg_color: "191724",
   }
+  thiagonox:{
+    title_color: "F22738",
+    icon_color:"A429A6",
+    text_color:"ffffff",
+    border_color:"A429A6",
+    bg_color:"0D0D0D"
+  }
 };
 
 module.exports = themes;
