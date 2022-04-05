b454fabbc33d8e3465dfd5c6b8b5785deddd31f8
jgarza9788 <jgarza9788@gmail.com>
2022-04-05 15:17:19 -0700

    001

--- api/index.js
+++ api/index.js
@@ -7,7 +7,8 @@ const {
   CONSTANTS,
 } = require("../src/common/utils");
 const fetchStats = require("../src/fetchers/stats-fetcher");
-const renderStatsCard = require("../src/cards/stats-card");
+// const renderStatsCard = require("../src/cards/stats-card");
+const renderStatsCard = require("../src/cards/hackerrank-card");
 const blacklist = require("../src/common/blacklist");
 const { isLocaleAvailable } = require("../src/translations");
 
