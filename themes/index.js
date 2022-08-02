22b7ce1f746c372f7b7127a3aa96264c4cf3f382
Dylan Ravel <dylanm.ravel@gmail.com>
2022-08-02 09:44:36 -0600

    created theme

--- themes/index.js
+++ themes/index.js
@@ -360,6 +360,12 @@ const themes = {
     icon_color: "ebbcba",
     text_color: "e0def4",
     bg_color: "191724",
+  },
+  gradient: {
+    title_color: "fff",
+    icon_color: "fff",
+    text_color: "fff",
+    bg_color: "30,e96443,904e95",
   }
 };
 
