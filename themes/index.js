76ba8041a0df65cac31588ab9c4d99bc388d78fb
htcz0qtb <114434705+htcz0qtb@users.noreply.github.com>
2022-09-27 12:07:06 +0200

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -361,6 +361,12 @@ export const themes = {
     text_color: "e0def4",
     bg_color: "191724",
   },
+  roser_pine: {
+    title_color: "#9ccfd8",
+    icon_color: "#ebbcba",
+    text_color: "#e0def4",
+    bg_color: "191724",
+  },
 };
 
 export default themes;
