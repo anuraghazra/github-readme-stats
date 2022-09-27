9f638f85fa424704a6e05cd099469cbda12b07fa
rickstaa <rick.staa@outlook.com>
2022-09-27 17:39:45 +0200

    test: add additional e2e card tests

--- tests/e2e/e2e.test.js
+++ tests/e2e/e2e.test.js
@@ -4,7 +4,11 @@
 import { describe } from "@jest/globals";
 import axios from "axios";
 import dotenv from "dotenv";
+
 dotenv.config();
+// Script variables
+const REPO = "github-readme-stats";
+const USER = "anuraghazra";
 
 describe("Fetch Cards", () => {
   let VERCEL_PREVIEW_URL;
@@ -15,31 +19,69 @@ describe("Fetch Cards", () => {
 
   test("retrieve stats card", async () => {
     expect(VERCEL_PREVIEW_URL).toBeDefined();
+
+    // Check if the vercel preview instance is up and running.
     await expect(
       axios.get(`${VERCEL_PREVIEW_URL}/api?username=willianrod`),
     ).resolves.not.toThrow();
+
+    // Get the vercel preview stats card response.
+    const serverSvg = await axios.get(
+      `${VERCEL_PREVIEW_URL}/api?username=willianrod`,
+    );
+
+    // Check if stats card from deployment matches the stats card from local.
+    expect(serverSvg.data).toMatchSnapshot();
   });
 
   test("retrieve language card", async () => {
     expect(VERCEL_PREVIEW_URL).toBeDefined();
+
+    // Check if the vercel preview instance is up and running.
     await expect(
       axios.get(`${VERCEL_PREVIEW_URL}/api/top-langs/?username=willianrod`),
     ).resolves.not.toThrow();
+
+    // Get the vercel preview stats card response.
+    const serverSvg = await axios.get(
+      `${VERCEL_PREVIEW_URL}/api/top-langs/?username=willianrod`,
+    );
+
+    // Check if stats card from deployment matches the stats card from local.
+    expect(serverSvg.data).toMatchSnapshot();
   });
 
   test("retrieve WakaTime card", async () => {
     expect(VERCEL_PREVIEW_URL).toBeDefined();
+
+    // Check if the vercel preview instance is up and running.
     await expect(
       axios.get(`${VERCEL_PREVIEW_URL}/api/wakatime?username=willianrod`),
     ).resolves.not.toThrow();
+
+    // Get the vercel preview stats card response.
+    const serverSvg = await axios.get(
+      `${VERCEL_PREVIEW_URL}/api/wakatime?username=willianrod`,
+    );
+
+    // Check if stats card from deployment matches the stats card from local.
+    expect(serverSvg.data).toMatchSnapshot();
   });
 
   test("retrieve repo card", async () => {
     expect(VERCEL_PREVIEW_URL).toBeDefined();
+
+    // Check if the vercel preview instance is up and running.
     await expect(
-      axios.get(
-        `${VERCEL_PREVIEW_URL}/api/pin/?username=anuraghazra&repo=github-readme-stats`,
-      ),
+      axios.get(`${VERCEL_PREVIEW_URL}/api/pin/?username=${USER}&repo=${REPO}`),
     ).resolves.not.toThrow();
+
+    // Get the vercel preview stats card response.
+    const serverSvg = await axios.get(
+      `${VERCEL_PREVIEW_URL}/api/pin/?username=${USER}&repo=${REPO}`,
+    );
+
+    // Check if stats card from deployment matches the stats card from local.
+    expect(serverSvg.data).toMatchSnapshot();
   });
 });
