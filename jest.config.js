f8e32f9e0cbb8ee5d9d7ca8c896a8ef6d71a3379
Rick Staa <rick.staa@outlook.com>
2022-10-01 13:59:02 +0200

    ci: add e2e vercel test action (#2054)
    * ci: add e2e vercel test action
    
    This commit adds several end-to-end tests that can be used
    to test whether the Vercel Preview deployment successfully
    returns the cards.
    
    * test: add additional e2e card tests
    
    * test: improve e2e card tests
    
    This commit makes sure that the tests also check whether a valid cards
    are returned from the preview deployment.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ export default {
   transform: {},
   testEnvironment: "jsdom",
   coverageProvider: "v8",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/e2e/"],
+  modulePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/e2e/"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/tests/E2E/",
+  ],
 };
