41bb7ceb61830b594af0307321f8fdc2eaf60566
Hasbi Setiawan <64304827+hasbisetiawan@users.noreply.github.com>
2022-10-02 08:58:41 +0700

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -361,12 +361,6 @@ export const themes = {
     text_color: "e0def4",
     bg_color: "191724",
   },
-  kronuz: {
-    title_color: "9aa83a",
-    icon_color: "caa473",
-    text_color: "e8e6e5",
-    bg_color: "383838",
-  },
   darker: {
     title_color: "f1c410",
     icon_color: "2dcc68",
