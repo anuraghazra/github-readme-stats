37411579bb3506bad738343c6037e01ef8891268
Matteo Pierro <pierromatteo@gmail.com>
2022-10-08 08:10:23 +0300

    add test for two pages

--- tests/top-langs.test.js
+++ tests/top-langs.test.js
@@ -45,6 +45,60 @@ const data_langs = {
   },
 };
 
+const firstPage = {
+  data: {
+    user: {
+      repositories: {
+        nodes: [
+          {
+            languages: {
+              edges: [{ size: 150, node: { color: "#0f0", name: "HTML" } }],
+            },
+          },
+          {
+            languages: {
+              edges: [{ size: 100, node: { color: "#0f0", name: "HTML" } }],
+            },
+          },
+        ],
+        pageInfo: {
+          hasNextPage: true,
+          cursor: "cursor",
+        },
+      },
+    },
+  },
+};
+
+const secondPage = {
+  data: {
+    user: {
+      repositories: {
+        nodes: [
+          {
+            languages: {
+              edges: [
+                { size: 100, node: { color: "#0ff", name: "javascript" } },
+              ],
+            },
+          },
+          {
+            languages: {
+              edges: [
+                { size: 100, node: { color: "#0ff", name: "javascript" } },
+              ],
+            },
+          },
+        ],
+        pageInfo: {
+          hasNextPage: false,
+          cursor: "cursor",
+        },
+      },
+    },
+  },
+};
+
 const error = {
   errors: [
     {
@@ -87,6 +141,29 @@ describe("Test /api/top-langs", () => {
       send: jest.fn(),
     };
     mock.onPost("https://api.github.com/graphql").reply(200, data_langs);
+
+    await topLangs(req, res);
+
+    expect(res.setHeader).toBeCalledWith("Content-Type", "image/svg+xml");
+    expect(res.send).toBeCalledWith(renderTopLanguages(langs));
+  });
+
+  it("should test the request with two pages", async () => {
+    const req = {
+      query: {
+        username: "anuraghazra",
+      },
+    };
+    const res = {
+      setHeader: jest.fn(),
+      send: jest.fn(),
+    };
+
+    mock
+      .onPost("https://api.github.com/graphql")
+      .replyOnce(200, firstPage)
+      .onPost("https://api.github.com/graphql")
+      .replyOnce(200, secondPage);
 
     await topLangs(req, res);
 
