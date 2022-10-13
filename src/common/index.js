f7a2b2162b62ccc77aae78d1405b58953ddc72ec
rickstaa <rick.staa@outlook.com>
2022-10-13 10:50:32 +0200

    fix: fix I18n import error

--- src/common/index.js
+++ src/common/index.js
@@ -1,7 +1,7 @@
 export { blacklist } from "./blacklist.js";
 export { Card } from "./Card.js";
 export { createProgressNode } from "./createProgressNode.js";
-export { i18n } from "./i18n.js";
+export { I18n } from "./I18n.js";
 export { icons } from "./icons.js";
 export { retryer } from "./retryer.js";
 export {
