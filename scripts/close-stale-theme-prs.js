1a4e5a69551c249a2eeaeb31f066718b2b104b82
Rick Staa <rick.staa@outlook.com>
2022-11-18 11:06:24 +0100

    ci: update theme stale close message

--- scripts/close-stale-theme-prs.js
+++ scripts/close-stale-theme-prs.js
@@ -10,7 +10,7 @@ import { RequestError } from "@octokit/request-error";
 import { getGithubToken, getRepoInfo } from "./helpers.js";
 
 const CLOSING_COMMENT = `
-	\rThis theme PR has been automatically closed due to inactivity. Please reopen it if you need to continue working on it.\
+	\rThis theme PR has been automatically closed due to inactivity. Please reopen it if you want to continue working on it.\
 	\rThank you for your contributions.
 `;
 const REVIEWER = "github-actions[bot]";
