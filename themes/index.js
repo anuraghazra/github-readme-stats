bbb04d8068af3a0c725b7f2f5f069e7506998bba
Sultan <99334334+sultansse@users.noreply.github.com>
2022-12-16 20:51:26 +0600

    added \ updated github_dark theme

--- themes/index.js
+++ themes/index.js
@@ -374,6 +374,13 @@ export const themes = {
     border_color: "170F0C",
     bg_color: "170F0C",
   },
+    github_dark: {
+    title_color: "1e6eea",
+    text_color: "39d353",
+    icon_color: "fefefe",
+    border_color: "39d353",
+    bg_color: "0d1117",
+  },
 };
 
 export default themes;
