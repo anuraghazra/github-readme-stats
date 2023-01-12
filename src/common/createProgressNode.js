c55e19bee71a960010c634d4e90621f42ad14997
Rick Staa <rick.staa@outlook.com>
2023-01-12 22:50:58 +0100

    Revert "Add loading Animation to Most used Language card (#2197)"
    This reverts commit 77dcdab42c306fd49e71e32a2335d6c06a90baf1.

--- src/common/createProgressNode.js
+++ src/common/createProgressNode.js
@@ -10,7 +10,6 @@ import { clampValue } from "./utils.js";
  * @param {string} createProgressNodeParams.color Progress color.
  * @param {string} createProgressNodeParams.progress Progress value.
  * @param {string} createProgressNodeParams.progressBarBackgroundColor Progress bar bg color.
- * @param {number} createProgressNodeParams.delay Delay before animation starts.
  * @returns {string} Progress node.
  */
 const createProgressNode = ({
@@ -20,22 +19,20 @@ const createProgressNode = ({
   color,
   progress,
   progressBarBackgroundColor,
-  delay,
 }) => {
   const progressPercentage = clampValue(progress, 2, 100);
 
   return `
     <svg width="${width}" x="${x}" y="${y}">
       <rect rx="5" ry="5" x="0" y="0" width="${width}" height="8" fill="${progressBarBackgroundColor}"></rect>
-      <svg data-testid="lang-progress" width="${progressPercentage}%">
-        <rect
-            height="8"
-            fill="${color}"
-            rx="5" ry="5" x="0" y="0"
-            class="lang-progress"
-            style="animation-delay: ${delay}ms;"
-        />
-      </svg>
+      <rect
+          height="8"
+          fill="${color}"
+          rx="5" ry="5" x="0" y="0"
+          data-testid="lang-progress"
+          width="${progressPercentage}%"
+      >
+      </rect>
     </svg>
   `;
 };
