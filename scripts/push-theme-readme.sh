70f0264905d9370efa635b6a611c8719b8958efa
Rick Staa <rick.staa@outlook.com>
2023-01-12 22:48:28 +0100

    Revert "ci: fix theme readme generation action (#2271)" (#2394)
    This reverts commit 5ae959b57c0831bc79d43425e07d0fe7c6a58011.

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -9,6 +9,6 @@ git config --global user.name "GitHub Readme Stats Bot"
 git branch -d $BRANCH_NAME || true
 git checkout -b $BRANCH_NAME
 git add --all
-git commit --no-verify --message "docs(theme): Auto update theme readme"
+git commit --message "docs(theme): Auto update theme readme" || exit 0
 git remote add origin-$BRANCH_NAME https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
 git push --force --quiet --set-upstream origin-$BRANCH_NAME $BRANCH_NAME
