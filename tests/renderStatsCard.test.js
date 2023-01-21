cfa84232e2cec0ddaed4a54699cfb8a7311e528e
rickstaa <rick.staa@outlook.com>
2023-01-21 11:32:25 +0100

    test: update snapshots

--- tests/renderStatsCard.test.js
+++ tests/renderStatsCard.test.js
@@ -329,7 +329,7 @@ describe("Test renderStatsCard", () => {
       document.querySelector(
         'g[transform="translate(0, 25)"]>.stagger>.stat.bold',
       ).textContent,
-    ).toMatchInlineSnapshot(`"累计提交数（commit） (2022):"`);
+    ).toMatchInlineSnapshot(`"累计提交数（commit） (2023):"`);
     expect(
       document.querySelector(
         'g[transform="translate(0, 50)"]>.stagger>.stat.bold',
