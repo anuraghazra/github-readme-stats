777202e8c07f8b67ef3aefcb34f25cd765d6ccf0
LUISMAGAIVER341 <84512640+LUISMAGAIVER341@users.noreply.github.com>
2023-01-22 13:33:35 -0300

    Update index.js
    Plz, add a <canvas id="canvas-basic"></canvas> on the card html, cuz the gradient theme need it.

--- themes/index.js
+++ themes/index.js
@@ -371,9 +371,30 @@ export const themes = {
     title_color: "DA7885",
     text_color: "E1B2A2",
     icon_color: "BB8470",
-    border_color: "170F0C",
+    border_color: "170F0C",  
     bg_color: "170F0C",
   },
+  gradient: {
+    title_color: "DA7885",
+    text_color: "E1B2A2",
+    icon_color: "BB8470",
+    border_color: "170F0C",
+  var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
+  
+  }
 };
 
 export default themes;
