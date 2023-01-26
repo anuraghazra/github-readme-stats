20914797b0d780485a8f13d762042b3c8e254b55
Zohan Subhash <zohan.subhash@gmail.com>
2023-01-26 16:52:18 +0530

    Disable cache

--- api/wild/pin.js
+++ api/wild/pin.js
@@ -14,7 +14,6 @@ export default async (req, res) => {
     text_color,
     bg_color,
     theme,
-    cache_seconds,
     border_radius,
     border_color,
   } = req.query;
@@ -34,34 +33,6 @@ export default async (req, res) => {
       highlight: req.query.highlight || "AWESOME",
     };
 
-    let cacheSeconds = clampValue(
-      parseInt(cache_seconds || CONSTANTS.FOUR_HOURS, 10),
-      CONSTANTS.FOUR_HOURS,
-      CONSTANTS.ONE_DAY,
-    );
-
-    /*
-      if star count & fork count is over 1k then we are kFormating the text
-      and if both are zero we are not showing the stats
-      so we can just make the cache longer, since there is no need to frequent updates
-    */
-    const stars = repoData.starCount;
-    const forks = repoData.forkCount;
-    if (!isNaN(stars) && !isNaN(forks)) {
-      const isBothOver1K = stars > 1000 && forks > 1000;
-      const isBothUnder1 = stars < 1 && forks < 1;
-      if (!cache_seconds && (isBothOver1K || isBothUnder1)) {
-        cacheSeconds = CONSTANTS.FOUR_HOURS;
-      }
-    }
-
-    res.setHeader(
-      "Cache-Control",
-      `max-age=${
-        cacheSeconds / 2
-      }, s-maxage=${cacheSeconds}, stale-while-revalidate=${CONSTANTS.ONE_DAY}`,
-    );
-
     return res.send(
       renderRepoCard(repoData, {
         hide_border: parseBoolean(hide_border),
@@ -75,7 +46,6 @@ export default async (req, res) => {
       }),
     );
   } catch (err) {
-    res.setHeader("Cache-Control", `no-cache, no-store, must-revalidate`); // Don't cache error responses.
     return res.send(renderError(err.message, err.secondaryMessage));
   }
 };
