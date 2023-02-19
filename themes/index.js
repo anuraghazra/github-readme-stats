376eb45bb03605a9500963fb15182ddbadbfef47
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-19 11:50:37 +0530

    Test
    Just want to see if we can make the themes have a transparent background.

--- themes/index.js
+++ themes/index.js
@@ -386,21 +386,21 @@ export const themes = {
     text_color: "444",
     icon_color: "4F0000",
     border_color: "4F0000",
-    bg_color: "1116",
+    bg_color: "ffffff00",
   },
   shadow_green: {
     title_color: "007A00",
     text_color: "444",
     icon_color: "003D00",
     border_color: "003D00",
-    bg_color: "1116",
+    bg_color: "ffffff00",
   },
   shadow_blue: {
     title_color: "00779A",
     text_color: "444",
     icon_color: "004450",
     border_color: "004490",
-    bg_color: "1116",
+    bg_color: "ffffff00",
   },
 };
 
