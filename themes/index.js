anuraghazra/github-readme-stats

commit a6ff0fa521f87f112f124bc7a9d0b435d4d166b4

Add one_dark_pro (#2507)
This colors were taken from One Dark Pro theme in VSCode extention. Please add it and I will use it in my profile

Co-authored-by: Zohan Subhash <zohan.subhash@gmail.com>

themes/index.js
@@ -374,6 +374,12 @@ export const themes = {
     border_color: "170F0C",
     bg_color: "170F0C",
   },
+  one_dark_pro: {
+    title_color: "61AFEF",
+    text_color: "E5C06E",
+    icon_color: "C678DD",
+    border_color: "3B4048",
+    bg_color: "23272E",
   rose: {
     title_color: "8d192b",
     text_color: "862931",

commit 6b380b7fa9fc2363336aea36673321fae22283a2

fix: fix JSON themes bug.
This fixes a JSON bug that was introduced in https://github.com/anuraghazra/github-readme-stats/pull/2507.

themes/index.js
@@ -380,6 +380,7 @@ export const themes = {
     icon_color: "C678DD",
     border_color: "3B4048",
     bg_color: "23272E",
+  },
   rose: {
     title_color: "8d192b",
     text_color: "862931",