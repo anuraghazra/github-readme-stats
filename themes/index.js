c15182283979a6f17f4c7a49a608149015e8d04b
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-25 13:44:47 +0530

    Revert "add holi_theme (#2539)"
    This reverts commit 5f20e6c97a35d77c4b2839b340bfc188ed23a056.

--- themes/index.js
+++ themes/index.js
@@ -387,13 +387,6 @@ export const themes = {
     border_color: "e9d8d4",
     bg_color: "e9d8d4",
   },
-  holi_theme: {
-    title_color: "5FABEE",
-    text_color: "D6E7FF",
-    icon_color: "5FABEE",
-    border_color: "85A4C0",
-    bg_color: "030314",
-  },
 };
 
 export default themes;
