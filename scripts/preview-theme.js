911bb99f1204536bf82e598c74a6a5bf65a830ec
Zohan Subhash <zohan.subhash@gmail.com>
2023-03-01 12:53:29 +0530

    Fix octokit error

--- scripts/preview-theme.js
+++ scripts/preview-theme.js
@@ -559,6 +559,7 @@ export const run = async (prNumber) => {
     }
   } catch (error) {
     debug("Set review state to `REQUEST_CHANGES` and add `invalid` label...");
+    const octokit = github.getOctokit(getGithubToken());
     if (!DRY_RUN) {
       await addReview(
         octokit,
