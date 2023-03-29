888876033d738f60214941ab7446220f71a15488
沐霖 <niefee@niefee.com>
2023-03-29 17:54:44 +0800

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,31 @@
+var https = require('follow-redirects').https;
+var fs = require('fs');
 export default function handler(req, res) {
+  var options = {
+    'method': 'POST',
+    'hostname': 'api.vercel.com',
+    'path': '/v1/integrations/deploy/prj_J2bHH9cKhtUEW2mUS1bdtDZFnO9r/cbVzW3rL0r',
+    'headers': {
+    },
+    'maxRedirects': 20
+  };
+
+  var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+
+    res.on("error", function (error) {
+      console.error(error);
+    });
+  });
+  req.end();
   res.status(200).end('Hello Cron!');
 }
