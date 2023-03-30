f0ee492eefebdbc51fa3d3d2ea58cd04d3de5294
沐霖 <niefee@niefee.com>
2023-03-30 09:28:03 +0800

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,30 +1,4 @@
-import { https } from 'follow-redirects'
 export default function handler(req, res) {
-  var options = {
-    'method': 'POST',
-    'hostname': 'api.vercel.com',
-    'path': '/v1/integrations/deploy/prj_J2bHH9cKhtUEW2mUS1bdtDZFnO9r/cbVzW3rL0r',
-    'headers': {
-    },
-    'maxRedirects': 20
-  };
-
-  var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-
-    res.on("end", function (chunk) {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-
-    res.on("error", function (error) {
-      console.error(error);
-    });
-  });
   req.end();
   res.status(200).end('Hello Cron!');
 }
