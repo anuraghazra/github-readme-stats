72be4738a74452bb13ba626a0cd5603c1123668c
Adolfo Sánchez Lopéz <81250601+Adolfsan99@users.noreply.github.com>
2023-04-29 16:10:07 -0500

    Update index.js
    add theme "github default colors with transparency"

--- themes/index.js
+++ themes/index.js
@@ -423,6 +423,12 @@ export const themes = {
     border_color: "85A4C0",
     bg_color: "030314",
   },
+  github_default_trans: {
+    title_color: "d1d1d1",
+    icon_color: "f78166",
+    text_color: "e6edf3",
+    bg_color: "ffffff00",
+  },
 };
 
 export default themes;
