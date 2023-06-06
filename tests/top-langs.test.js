223e47e5aa50ca69830b438fd287ce2dc3a09e0b
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2023-06-06 09:56:23 +0300

    Cover with test changes in #2770 pull request (#2775)

--- tests/top-langs.test.js
+++ tests/top-langs.test.js
@@ -123,7 +123,7 @@ describe("Test /api/top-langs", () => {
     );
   });
 
-  it("should render error card on error", async () => {
+  it("should render error card on user data fetch error", async () => {
     const req = {
       query: {
         username: "anuraghazra",
@@ -139,5 +139,26 @@ describe("Test /api/top-langs", () => {
 
     expect(res.setHeader).toBeCalledWith("Content-Type", "image/svg+xml");
     expect(res.send).toBeCalledWith(renderError(error.errors[0].message));
+  });
+
+  it("should render error card on incorrect layout input", async () => {
+    const req = {
+      query: {
+        username: "anuraghazra",
+        layout: ["pie"],
+      },
+    };
+    const res = {
+      setHeader: jest.fn(),
+      send: jest.fn(),
+    };
+    mock.onPost("https://api.github.com/graphql").reply(200, data_langs);
+
+    await topLangs(req, res);
+
+    expect(res.setHeader).toBeCalledWith("Content-Type", "image/svg+xml");
+    expect(res.send).toBeCalledWith(
+      renderError("Something went wrong", "Incorrect layout input"),
+    );
   });
 });
