ec622256c00b52e8b52b8825cbb2a04087c17aff
Rick Staa <rick.staa@outlook.com>
2023-06-15 22:25:07 +0200

    Revert "Use default json import (#2820)" (#2829)
    This reverts commit 1474a3abbee179d955f7eb445230716e47e1e68e.

--- src/cards/wakatime-card.js
+++ src/cards/wakatime-card.js
@@ -10,7 +10,17 @@ import {
 } from "../common/utils.js";
 import { getStyles } from "../getStyles.js";
 import { wakatimeCardLocales } from "../translations.js";
-import languageColors from "../common/languageColors.json";
+
+/** Import language colors.
+ *
+ * @description Here we use the workaround found in
+ * https://stackoverflow.com/questions/66726365/how-should-i-import-json-in-node
+ * since vercel is using v16.14.0 which does not yet support json imports without the
+ * --experimental-json-modules flag.
+ */
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const languageColors = require("../common/languageColors.json"); // now works
 
 /**
  * Creates the no coding activity SVG node.
