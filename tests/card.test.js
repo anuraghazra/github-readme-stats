d6cc0511d012322074baeb4f7f1d4974636cb904
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2023-09-04 11:11:28 +0300

    tests: add missing card set title test (#3190)

--- tests/card.test.js
+++ tests/card.test.js
@@ -41,6 +41,16 @@ describe("Card", () => {
     );
   });
 
+  it("should set custom title", () => {
+    const card = new Card({});
+    card.setTitle("custom title");
+
+    document.body.innerHTML = card.render(``);
+    expect(queryByTestId(document.body, "card-title")).toHaveTextContent(
+      "custom title",
+    );
+  });
+
   it("should hide title", () => {
     const card = new Card({});
     card.setHideTitle(true);
