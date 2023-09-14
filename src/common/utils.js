b9681f7cfc378852f504d338a714f28512d92542
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2023-09-14 11:47:00 +0300

    refactor: use more clear retryer error message (2) (#3227)

--- src/common/utils.js
+++ src/common/utils.js
@@ -383,7 +383,8 @@ const CONSTANTS = {
 const OWNER_AFFILIATIONS = ["OWNER", "COLLABORATOR", "ORGANIZATION_MEMBER"];
 
 const SECONDARY_ERROR_MESSAGES = {
-  MAX_RETRY: "Downtime due to GitHub API rate limiting",
+  MAX_RETRY:
+    "You can deploy own instance or wait until public will be no longer limited",
   NO_TOKENS:
     "Please add an env variable called PAT_1 with your GitHub API token in vercel",
   USER_NOT_FOUND: "Make sure the provided username is not an organization",
