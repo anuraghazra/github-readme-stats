bb3cb9abc1eb45bcced010e14224b04640efdad5
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2023-09-15 00:44:49 +0200

    allow usage of .env file for PAT tokens

--- express.js
+++ express.js
@@ -1,12 +1,11 @@
+import "dotenv/config";
 import statsCard from "./api/index.js";
 import repoCard from "./api/pin.js";
 import langCard from "./api/top-langs.js";
 import wakatimeCard from "./api/wakatime.js";
 import gistCard from "./api/gist.js";
 import express from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
 app.listen(process.env.port || 9000);
 
