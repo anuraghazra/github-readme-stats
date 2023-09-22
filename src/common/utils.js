7bf4a873f1012865594fde8d740fd6ed8efb568b
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2023-09-22 18:43:58 +0300

    refactor: add GitHub REST API error to CustomError class (#3272)

--- src/common/utils.js
+++ src/common/utils.js
@@ -399,13 +399,16 @@ const CONSTANTS = {
   ERROR_CACHE_SECONDS: TEN_MINUTES,
 };
 
+const TRY_AGAING_LATER = "Please try again later";
+
 const SECONDARY_ERROR_MESSAGES = {
   MAX_RETRY:
     "You can deploy own instance or wait until public will be no longer limited",
   NO_TOKENS:
     "Please add an env variable called PAT_1 with your GitHub API token in vercel",
   USER_NOT_FOUND: "Make sure the provided username is not an organization",
-  GRAPHQL_ERROR: "Please try again later",
+  GRAPHQL_ERROR: TRY_AGAING_LATER,
+  GITHUB_REST_API_ERROR: TRY_AGAING_LATER,
   WAKATIME_USER_NOT_FOUND: "Make sure you have a public WakaTime profile",
 };
 
@@ -427,6 +430,7 @@ class CustomError extends Error {
   static NO_TOKENS = "NO_TOKENS";
   static USER_NOT_FOUND = "USER_NOT_FOUND";
   static GRAPHQL_ERROR = "GRAPHQL_ERROR";
+  static GITHUB_REST_API_ERROR = "GITHUB_REST_API_ERROR";
   static WAKATIME_ERROR = "WAKATIME_ERROR";
 }
 
