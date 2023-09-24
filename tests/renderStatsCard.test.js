2485d27d3a25bc91bf9a2cd3d8ebfd8bac11390a
FajarKim <penghapusjawaban@gmail.com>
2023-09-24 12:46:30 +0700

    feat: Add trophy rank icon

--- tests/renderStatsCard.test.js
+++ tests/renderStatsCard.test.js
@@ -451,6 +451,13 @@ describe("Test renderStatsCard", () => {
     ).toBe(stats.rank.percentile.toFixed(1) + "%");
   });
 
+  it("should render trophy rank icon", () => {
+    document.body.innerHTML = renderStatsCard(stats, {
+      rank_icon: "trophy",
+    });
+    expect(queryByTestId(document.body, "trophy-rank-icon")).toBeDefined();
+  });
+
   it("should throw error if all stats and rank icon are hidden", () => {
     expect(() =>
       renderStatsCard(stats, {
