5a79fe1340c3a127a4c5bfc4ba8a266265d55a62
sahilpawar01 <146012578+sahilpawar01@users.noreply.github.com>
2023-10-01 18:08:32 +0530

    Update generate-theme-doc.js
    Optimize README generation code for efficiency and readability.

--- scripts/generate-theme-doc.js
+++ scripts/generate-theme-doc.js
@@ -2,11 +2,14 @@ import fs from "fs";
 import { themes } from "../themes/index.js";
 
 const TARGET_FILE = "./themes/README.md";
-const REPO_CARD_LINKS_FLAG = "<!-- REPO_CARD_LINKS -->";
-const STAT_CARD_LINKS_FLAG = "<!-- STATS_CARD_LINKS -->";
-
-const STAT_CARD_TABLE_FLAG = "<!-- STATS_CARD_TABLE -->";
-const REPO_CARD_TABLE_FLAG = "<!-- REPO_CARD_TABLE -->";
+const LINKS_FLAG_MAP = {
+  repo: "<!-- REPO_CARD_LINKS -->",
+  stats: "<!-- STATS_CARD_LINKS -->",
+};
+const TABLE_FLAG_MAP = {
+  repo: "<!-- REPO_CARD_TABLE -->",
+  stats: "<!-- STATS_CARD_TABLE -->",
+};
 
 const THEME_TEMPLATE = `## Available Themes
 
@@ -14,7 +17,7 @@ const THEME_TEMPLATE = `## Available Themes
 
 With inbuilt themes, you can customize the look of the card without doing any manual customization.
 
-Use \`?theme=THEME_NAME\` parameter like so :-
+Use \`?theme=THEME_NAME\` parameter like so: 
 
 \`\`\`md
 ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=anuraghazra&theme=dark&show_icons=true)
@@ -26,7 +29,7 @@ Use \`?theme=THEME_NAME\` parameter like so :-
 
 | | | |
 | :--: | :--: | :--: |
-${STAT_CARD_TABLE_FLAG}
+<!-- STATS_CARD_TABLE -->
 
 ## Repo Card
 
@@ -34,61 +37,51 @@ ${STAT_CARD_TABLE_FLAG}
 
 | | | |
 | :--: | :--: | :--: |
-${REPO_CARD_TABLE_FLAG}
+<!-- REPO_CARD_TABLE -->
 
-${STAT_CARD_LINKS_FLAG}
-
-${REPO_CARD_LINKS_FLAG}
-
+<!-- STATS_CARD_LINKS -->
+<!-- REPO_CARD_LINKS -->
 
 [add-theme]: https://github.com/anuraghazra/github-readme-stats/edit/master/themes/index.js
 
 Want to add a new theme? Consider reading the [contribution guidelines](../CONTRIBUTING.md#themes-contribution) :D
 `;
 
-const createRepoMdLink = (theme) => {
-  return `\n[${theme}_repo]: https://github-readme-stats.vercel.app/api/pin/?username=anuraghazra&repo=github-readme-stats&cache_seconds=86400&theme=${theme}`;
-};
-const createStatMdLink = (theme) => {
-  return `\n[${theme}]: https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true&hide=contribs,prs&cache_seconds=86400&theme=${theme}`;
+const createMdLink = (theme, type) => {
+  const baseLink = type === "repo" ? "github-readme-stats/api/pin/" : "github-readme-stats/";
+  return `\n[${theme}]: https://github-readme-stats.vercel.app/${baseLink}?username=anuraghazra&cache_seconds=86400&theme=${theme}`;
 };
 
-const generateLinks = (fn) => {
+const generateLinks = (type) => {
   return Object.keys(themes)
-    .map((name) => fn(name))
+    .map((name) => createMdLink(name, type))
     .join("");
 };
 
-const createTableItem = ({ link, label, isRepoCard }) => {
+const createTableItem = ({ link, label }) => {
   if (!link || !label) {
     return "";
   }
-  return `\`${label}\` ![${link}][${link}${isRepoCard ? "_repo" : ""}]`;
+  return `\`${label}\` ![${link}][${link}]`;
 };
 
 const generateTable = ({ isRepoCard }) => {
   const rows = [];
   const themesFiltered = Object.keys(themes).filter(
-    (name) => name !== (!isRepoCard ? "default_repocard" : "default"),
+    (name) => name !== (!isRepoCard ? "default_repocard" : "default")
   );
 
   for (let i = 0; i < themesFiltered.length; i += 3) {
-    const one = themesFiltered[i];
-    const two = themesFiltered[i + 1];
-    const three = themesFiltered[i + 2];
+    const [one, two, three] = themesFiltered.slice(i, i + 3);
 
-    let tableItem1 = createTableItem({ link: one, label: one, isRepoCard });
-    let tableItem2 = createTableItem({ link: two, label: two, isRepoCard });
-    let tableItem3 = createTableItem({ link: three, label: three, isRepoCard });
+    const tableItem1 = createTableItem({ link: one, label: one });
+    const tableItem2 = createTableItem({ link: two, label: two });
+    const tableItem3 = createTableItem({ link: three, label: three });
 
-    if (three === undefined) {
-      tableItem3 = `[Add your theme][add-theme]`;
-    }
-    rows.push(`| ${tableItem1} | ${tableItem2} | ${tableItem3} |`);
-
-    // if it's the last row & the row has no empty space push a new row
-    if (three && i + 3 === themesFiltered.length) {
+    if (!three) {
       rows.push(`| [Add your theme][add-theme] | | |`);
+    } else {
+      rows.push(`| ${tableItem1} | ${tableItem2} | ${tableItem3} |`);
     }
   }
 
@@ -98,16 +91,16 @@ const generateTable = ({ isRepoCard }) => {
 const buildReadme = () => {
   return THEME_TEMPLATE.split("\n")
     .map((line) => {
-      if (line.includes(REPO_CARD_LINKS_FLAG)) {
-        return generateLinks(createRepoMdLink);
+      if (line.includes(LINKS_FLAG_MAP.repo)) {
+        return generateLinks("repo");
       }
-      if (line.includes(STAT_CARD_LINKS_FLAG)) {
-        return generateLinks(createStatMdLink);
+      if (line.includes(LINKS_FLAG_MAP.stats)) {
+        return generateLinks("stats");
       }
-      if (line.includes(REPO_CARD_TABLE_FLAG)) {
+      if (line.includes(TABLE_FLAG_MAP.repo)) {
         return generateTable({ isRepoCard: true });
       }
-      if (line.includes(STAT_CARD_TABLE_FLAG)) {
+      if (line.includes(TABLE_FLAG_MAP.stats)) {
         return generateTable({ isRepoCard: false });
       }
       return line;
@@ -116,3 +109,6 @@ const buildReadme = () => {
 };
 
 fs.writeFileSync(TARGET_FILE, buildReadme());
+
+console.log("README.md updated successfully!");
+
