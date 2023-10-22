e272eb73ad3731051e5dfe447372970de1209011
Yaten Dhingra <yaten598@gmail.com>
2023-10-23 00:58:29 +0530

    feat: added  argument to gist card

--- src/cards/gist-card.js
+++ src/cards/gist-card.js
@@ -54,6 +54,7 @@ const renderGistCard = (gistData, options = {}) => {
     border_color,
     show_owner = false,
     hide_border = false,
+    card_width,
   } = options;
 
   // returns theme based colors with proper overrides and defaults
@@ -118,7 +119,7 @@ const renderGistCard = (gistData, options = {}) => {
         ? `${header.slice(0, HEADER_MAX_LENGTH)}...`
         : header,
     titlePrefixIcon: icons.gist,
-    width: CARD_DEFAULT_WIDTH,
+    width: card_width ? card_width : CARD_DEFAULT_WIDTH,
     height,
     border_radius,
     colors: {
