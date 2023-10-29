ad17713e488e0ab28c49cf59d99d77d9b3e6ac93
Chongyi Zheng <git@zcy.dev>
2023-10-28 21:25:24 -0400

    feature: add all other routes to cloudflare workers

--- cloudflare/main.js
+++ cloudflare/main.js
@@ -1,12 +1,36 @@
-import index from "./index.js";
+import { RequestAdapter, ResponseAdapter } from "./adapter.js";
+import gist from "../api/gist.js";
+import index from "../api/index.js";
+import pin from "../api/pin.js";
+import topLangs from "../api/top-langs.js";
+import wakatime from "../api/wakatime.js";
+import statusPatInfo from "../api/status/pat-info.js";
+import statusUp from "../api/status/up.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const req = new RequestAdapter(request);
+    const res = new ResponseAdapter();
+
     const { pathname } = new URL(request.url);
     if (pathname === "/") {
-      return index.fetch(request, env, ctx);
+      await index(req, res);
+    } else if (pathname === "/gist") {
+      await gist(req, res);
+    } else if (pathname === "/pin") {
+      await pin(req, res);
+    } else if (pathname === "/top-langs") {
+      await topLangs(req, res);
+    } else if (pathname === "/wakatime") {
+      await wakatime(req, res);
+    } else if (pathname === "/status/pat-info") {
+      await statusPatInfo(req, res);
+    } else if (pathname === "/status/up") {
+      await statusUp(req, res);
+    } else {
+      return new Response("not found", { status: 404 });
     }
 
-    return new Response("not found", { status: 404 });
+    return res.toResponse();
   },
 };
