1785159aac14711c5378377a52ec13ae027671a7
Chongyi Zheng <git@zcy.dev>
2023-10-30 16:22:31 -0400

    Remove deployment prep workflow (#7)

--- .github/workflows/deploy-prep.py
+++
@@ -1,8 +0,0 @@
-file = open('./vercel.json', 'r')
-str = file.read()
-file = open('./vercel.json', 'w')
-
-str = str.replace('"maxDuration": 10', '"maxDuration": 30')
-
-file.write(str)
-file.close()
