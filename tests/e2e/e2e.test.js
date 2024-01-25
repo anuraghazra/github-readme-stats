29a8704fcc70c2366261b251f019e9d0669917fc
Alexandr <qwerty541zxc@gmail.com>
2024-01-25 11:09:28 +0200

    tests: refactor stats card e2e test to avoid future error due to yearly data update

--- tests/e2e/e2e.test.js
+++ tests/e2e/e2e.test.js
@@ -120,11 +120,13 @@ describe("Fetch Cards", () => {
     ).resolves.not.toThrow();
 
     // Get local stats card.
-    const localStatsCardSVG = renderStatsCard(STATS_DATA);
+    const localStatsCardSVG = renderStatsCard(STATS_DATA, {
+      include_all_commits: true,
+    });
 
     // Get the Vercel preview stats card response.
     const serverStatsSvg = await axios.get(
-      `${VERCEL_PREVIEW_URL}/api?username=${USER}&${CACHE_BURST_STRING}`,
+      `${VERCEL_PREVIEW_URL}/api?username=${USER}&include_all_commits=true&${CACHE_BURST_STRING}`,
     );
 
     // Check if stats card from deployment matches the stats card from local.
