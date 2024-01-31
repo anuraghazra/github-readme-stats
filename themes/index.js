62e33dea02711f1684bdcd8fddace993bc4fe612
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2024-01-31 02:01:36 +0100

    Fixed syntax mistake
    Sorry, it's my first pull request to a big project.

--- themes/index.js
+++ themes/index.js
@@ -466,7 +466,7 @@ export const themes = {
     title_color: "00bda3",
     text_color: "b0a3e1",
     icon_color: "00c3ae",
-    border_color=b9edc3,
+    border_color: "b9edc3",
     bg_color: "dafbe1",
   },
 };
