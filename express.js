e80dacb176f8751bd45ef83d265f3d144364adc3
Andrew Stowell <andrew@digitaladapt.com>
2024-05-24 11:31:48 -0400

    initial functional docker setup for anuraghazra/github-readme-stats

--- express.js
+++ express.js
@@ -7,7 +7,16 @@ import gistCard from "./api/gist.js";
 import express from "express";
 
 const app = express();
-app.listen(process.env.port || 9000);
+const server = app.listen(process.env.port || 9000);
+console.log("started, awaiting requests");
+
+/* stop gracefully when requested */
+process.on("SIGTERM", () => {
+    console.log("stopping, received SIGTERM signal");
+    server.close(() => {
+        process.exit();
+    });
+});
 
 app.get("/", statsCard);
 app.get("/pin", repoCard);
