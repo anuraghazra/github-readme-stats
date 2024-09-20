ca72b73fbefd69a5a309961b41cf7ea88efba241
Kyle Upton <kyleaupton@gmail.com>
2024-09-20 16:39:25 -0400

    added gist whitelist

--- src/common/whitelist.js
+++ src/common/whitelist.js
@@ -2,5 +2,9 @@ const whitelist = process.env.WHITELIST
   ? process.env.WHITELIST.split(",")
   : undefined;
 
-export { whitelist };
+const gistWhitelist = process.env.GIST_WHITELIST
+  ? process.env.GIST_WHITELIST.split(",")
+  : undefined;
+
+export { whitelist, gistWhitelist };
 export default whitelist;
