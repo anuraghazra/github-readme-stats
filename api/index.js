cdc6ad1eaf1b12d08e5c7ed4a7e76fa51326eb61
anuraghazra <hazru.anurag@gmail.com>
2024-10-11 23:07:05 +0530

    feat: cost optimize, increase cache

--- api/index.js
+++ api/index.js
@@ -79,8 +79,8 @@ export default async (req, res) => {
 
     let cacheSeconds = clampValue(
       parseInt(cache_seconds || CONSTANTS.CARD_CACHE_SECONDS, 10),
-      CONSTANTS.SIX_HOURS,
-      CONSTANTS.ONE_DAY,
+      CONSTANTS.TWELVE_HOURS,
+      CONSTANTS.TWO_DAY,
     );
     cacheSeconds = process.env.CACHE_SECONDS
       ? parseInt(process.env.CACHE_SECONDS, 10) || cacheSeconds
