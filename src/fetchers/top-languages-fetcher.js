a25e0489373310f765a8e32321223a9ba9b2781e
爐㶮彪狐居士 <47691322+BiuwuLOK@users.noreply.github.com>
2025-03-20 12:02:19 +0800

    Update top-languages-fetcher.js
    By add new feature of `isArchived` then use on the URL as `&exclude repo=isArchived` will ignore all the Archived repos languages count. 
    
    This just personal needed, maybe someone could use it

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -27,7 +27,7 @@ const fetcher = (variables, token) => {
       query userInfo($login: String!) {
         user(login: $login) {
           # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 100, isArchived: false) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
