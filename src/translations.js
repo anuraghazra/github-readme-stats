30e9bdd5abc3ae084a17408f7ff9c4e8f7acb2df
Kornchanok Iednusorn <165641936+HKornchanok@users.noreply.github.com>
2025-05-07 22:45:36 +0700

    Add locale th to translations.js

--- src/translations.js
+++ src/translations.js
@@ -45,6 +45,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: `${encodedName}ning GitHub'dagi statistikasi`,
       vi: `Thống Kê GitHub ${encodedName}`,
       se: `GitHubstatistik för ${encodedName}`,
+      th: `สถิติ GitHub ของ ${encodedName}`,
     },
     "statcard.ranktitle": {
       ar: `${encodedName} إحصائيات غيت هاب`,
@@ -76,6 +77,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: `${encodedName}ning GitHub'dagi statistikasi`,
       vi: `Thống Kê GitHub ${encodedName}`,
       se: `GitHubstatistik för ${encodedName}`,
+      th: `อันดับ GitHub ของ ${encodedName}`,
     },
     "statcard.totalstars": {
       ar: "مجموع النجوم",
@@ -107,6 +109,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Yulduzchalar",
       vi: "Tổng Số Sao",
       se: "Antal intjänade stjärnor",
+      th: "ดาวทั้งหมดที่ได้รับ",
     },
     "statcard.commits": {
       ar: "مجموع الحفظ",
@@ -138,6 +141,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "'Commit'lar",
       vi: "Tổng Số Cam Kết",
       se: "Totalt antal commits",
+      th: "Commit ทั้งหมด",
     },
     "statcard.prs": {
       ar: "مجموع طلبات السحب",
@@ -169,6 +173,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "'Pull Request'lar",
       vi: "Tổng Số PR",
       se: "Totalt antal PR",
+      th: "PR ทั้งหมด",
     },
     "statcard.issues": {
       ar: "مجموع التحسينات",
@@ -200,6 +205,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "'Issue'lar",
       vi: "Tổng Số Vấn Đề",
       se: "Total antal issues",
+      th: "Issue ทั้งหมด",
     },
     "statcard.contribs": {
       ar: "ساهم في (العام الماضي)",
@@ -231,6 +237,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Hissa qoʻshgan (o'tgan yili)",
       vi: "Đã Đóng Góp (năm ngoái)",
       se: "Bidragit till (förra året)",
+      th: "มีส่วนร่วมใน (ปีที่แล้ว)",
     },
     "statcard.reviews": {
       ar: "تمت مراجعة إجمالي العلاقات العامة",
@@ -262,6 +269,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Koʻrib chiqilgan PR-lar soni",
       vi: "Tổng Số PR Đã Xem Xét",
       se: "Totalt antal granskade PR",
+      th: "รีวิว PR แล้วทั้งหมด",
     },
     "statcard.discussions-started": {
       ar: "مجموع بدء المناقشات",
@@ -293,6 +301,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Boshlangan muzokaralar soni",
       vi: "Tổng Số Thảo Luận Bắt Đầu",
       se: "Totalt antal diskussioner startade",
+      th: "เริ่มหัวข้อสนทนาทั้งหมด",
     },
     "statcard.discussions-answered": {
       ar: "مجموع الردود على المناقشات",
@@ -324,6 +333,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Javob berilgan muzokaralar soni",
       vi: "Tổng Số Thảo Luận Đã Trả Lời",
       se: "Totalt antal diskussioner besvarade",
+      th: "ตอบกลับหัวข้อสนทนาทั้งหมด",
     },
     "statcard.prs-merged": {
       ar: "مجموع الطلبات المدمجة",
@@ -354,6 +364,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Birlangan PR-lar soni",
       vi: "Tổng Số PR Đã Hợp Nhất",
       se: "Totalt antal sammanfogade PR",
+      th: "PR ที่ถูก Merged แล้วทั้งหมด",
     },
     "statcard.prs-merged-percentage": {
       ar: "نسبة الطلبات المدمجة",
@@ -384,6 +395,7 @@ const statCardLocales = ({ name, apostrophe }) => {
       uz: "Birlangan PR-lar foizi",
       vi: "Tỷ Lệ PR Đã Hợp Nhất",
       se: "Procent av sammanfogade PR",
+      th: "เปอร์เซ็นต์ PR ที่ถูก Merged แล้วทั้งหมด",
     },
   };
 };
@@ -419,6 +431,7 @@ const repoCardLocales = {
     uz: "Shablon",
     vi: "Mẫu",
     se: "Mall",
+    th: "เทมเพลต",
   },
   "repocard.archived": {
     ar: "محفوظ",
@@ -450,6 +463,7 @@ const repoCardLocales = {
     uz: "Arxivlangan",
     vi: "Đã Lưu Trữ",
     se: "Arkiverade",
+    th: "เก็บถาวร",
   },
 };
 
@@ -484,6 +498,7 @@ const langCardLocales = {
     uz: "Eng koʻp ishlatiladigan tillar",
     vi: "Ngôn Ngữ Thường Sử Dụng",
     se: "Mest använda språken",
+    th: "ภาษาที่ใช้บ่อยที่สุด",
   },
   "langcard.nodata": {
     ar: "لا توجد بيانات لغات.",
@@ -515,6 +530,7 @@ const langCardLocales = {
     uz: "Til haqida ma'lumot yo'q.",
     vi: "Không có dữ liệu ngôn ngữ.",
     se: "Inga språkdata.",
+    th: "ไม่มีข้อมูลภาษา",
   },
 };
 
@@ -549,6 +565,7 @@ const wakatimeCardLocales = {
     uz: "WakaTime statistikasi",
     vi: "Thống Kê WakaTime",
     se: "WakaTime statistik",
+    th: "สถิติ WakaTime",
   },
   "wakatimecard.lastyear": {
     ar: "العام الماضي",
@@ -580,6 +597,7 @@ const wakatimeCardLocales = {
     uz: "O'tgan yil",
     vi: "Năm ngoái",
     se: "Förra året",
+    th: "ปีที่แล้ว",
   },
   "wakatimecard.last7days": {
     ar: "آخر 7 أيام",
@@ -611,6 +629,7 @@ const wakatimeCardLocales = {
     uz: "O'tgan 7 kun",
     vi: "7 ngày qua",
     se: "Senaste 7 dagarna",
+    th: "7 วันที่ผ่านมา",
   },
   "wakatimecard.notpublic": {
     ar: "ملف المستخدم غير عام",
@@ -642,6 +661,7 @@ const wakatimeCardLocales = {
     uz: "WakaTime foydalanuvchi profili ochiq emas",
     vi: "Hồ sơ người dùng WakaTime không công khai",
     se: "WakaTime användarprofil inte offentlig",
+    th: "โปรไฟล์ผู้ใช้ WakaTime ไม่ได้เป็นสาธารณะ",
   },
   "wakatimecard.nocodedetails": {
     ar: "المستخدم لا يشارك معلومات تفصيلية عن البرمجة",
@@ -675,6 +695,7 @@ const wakatimeCardLocales = {
     uz: "Foydalanuvchi umumiy ko`d statistikasini ochiq ravishda almashmaydi",
     vi: "Người dùng không chia sẻ thống kê mã chi tiết công khai",
     se: "Användaren delar inte offentligt detaljerad kodstatistik",
+    th: "ผู้ใช้ไม่ได้แชร์สถิติโค้ดแบบสาธารณะ",
   },
   "wakatimecard.nocodingactivity": {
     ar: "لا يوجد نشاط برمجي لهذا الأسبوع",
@@ -706,6 +727,7 @@ const wakatimeCardLocales = {
     uz: "Bu hafta faol bo'lmadi",
     vi: "Không Có Hoạt Động Trong Tuần Này",
     se: "Ingen aktivitet denna vecka",
+    th: "ไม่มีกิจกรรมการเขียนโค้ดในสัปดาห์นี้",
   },
 };
 
