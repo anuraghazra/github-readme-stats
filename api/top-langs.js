8893c4a5d1c3a90754e81909ac49392b073ae04a
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2025-09-07 20:43:59 +0300

    fix: return cache seconds value clamping which was accidentally removed in cdc6ad1 (#4379)

--- api/top-langs.js
+++ api/top-langs.js
@@ -1,6 +1,7 @@
 import { renderTopLanguages } from "../src/cards/top-languages.js";
 import { blacklist } from "../src/common/blacklist.js";
 import {
+  clampValue,
   CONSTANTS,
   parseArray,
   parseBoolean,
@@ -70,9 +71,10 @@ export const handler = async (req, res, env) => {
       count_weight,
     );
 
-    let cacheSeconds = parseInt(
-      cache_seconds || CONSTANTS.TOP_LANGS_CACHE_SECONDS,
-      10,
+    let cacheSeconds = clampValue(
+      parseInt(cache_seconds || CONSTANTS.TOP_LANGS_CACHE_SECONDS, 10),
+      CONSTANTS.TWO_DAY,
+      CONSTANTS.TEN_DAY,
     );
     cacheSeconds = process.env.CACHE_SECONDS
       ? parseInt(process.env.CACHE_SECONDS, 10) || cacheSeconds
