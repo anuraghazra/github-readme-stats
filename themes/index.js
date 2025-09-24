9f02980a618a7eceb2204ea6385e2717fa5acd19
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-09-24 12:28:18 +0500

    Renamed theme name

--- themes/index.js
+++ themes/index.js
@@ -462,7 +462,7 @@ export const themes = {
     icon_color: "ffffff",
     bg_color: "35,4158d0,c850c0,ffcc70",
   },
-  pakistan: {
+  pak_Ahtisham: {
     title_color: "ffffff",
     text_color: "ffffff",
     icon_color: "ffffff",
