b5b41d3d07bf543fbe4f3b91fe0f830a3d7d95a4
Alexandr <qwerty541zxc@gmail.com>
2025-10-06 23:31:11 +0300

    dev

--- src/common/cache.js
+++ src/common/cache.js
@@ -3,12 +3,12 @@ import { clampValue } from "./utils";
 /**
  * Resolves the cache seconds based on the requested, default, min, and max values.
  *
- * @param {Object} args - The parameters object.
- * @param {number} args.requested - The requested cache seconds.
- * @param {number} args.def - The default cache seconds.
- * @param {number} args.min - The minimum cache seconds.
- * @param {number} args.max - The maximum cache seconds.
- * @returns {number} - The resolved cache seconds.
+ * @param {Object} args The parameters object.
+ * @param {number} args.requested The requested cache seconds.
+ * @param {number} args.def The default cache seconds.
+ * @param {number} args.min The minimum cache seconds.
+ * @param {number} args.max The maximum cache seconds.
+ * @returns {number} The resolved cache seconds.
  */
 const resolveCacheSeconds = ({ requested, def, min, max }) => {
   let cacheSeconds = clampValue(parseInt(requested || def, 10), min, max);
