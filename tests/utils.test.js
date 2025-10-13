c1d13eac198ba3eeccb13f2e02dfefb6b56fbfe3
Ophelia Goldstein <159258143+opheliagoldstein@users.noreply.github.com>
2025-10-13 20:39:16 +0300

    refactor: move color related tests into separate file (#4564)
    Co-authored-by: Alexandr <qwerty541zxc@gmail.com>

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -9,7 +9,6 @@ import {
   renderError,
   wrapTextMultiline,
 } from "../src/common/utils.js";
-import { getCardColors } from "../src/common/color.js";
 
 describe("Test utils.js", () => {
   it("should test kFormatter default behavior", () => {
@@ -101,78 +100,6 @@ describe("Test utils.js", () => {
     expect(
       queryByTestId(document.body, "message").children[1],
     ).toHaveTextContent(/Secondary Message/gim);
-  });
-
-  it("getCardColors: should return expected values", () => {
-    let colors = getCardColors({
-      title_color: "f00",
-      text_color: "0f0",
-      ring_color: "0000ff",
-      icon_color: "00f",
-      bg_color: "fff",
-      border_color: "fff",
-      theme: "dark",
-    });
-    expect(colors).toStrictEqual({
-      titleColor: "#f00",
-      textColor: "#0f0",
-      iconColor: "#00f",
-      ringColor: "#0000ff",
-      bgColor: "#fff",
-      borderColor: "#fff",
-    });
-  });
-
-  it("getCardColors: should fallback to default colors if color is invalid", () => {
-    let colors = getCardColors({
-      title_color: "invalidcolor",
-      text_color: "0f0",
-      icon_color: "00f",
-      bg_color: "fff",
-      border_color: "invalidColor",
-      theme: "dark",
-    });
-    expect(colors).toStrictEqual({
-      titleColor: "#2f80ed",
-      textColor: "#0f0",
-      iconColor: "#00f",
-      ringColor: "#2f80ed",
-      bgColor: "#fff",
-      borderColor: "#e4e2e2",
-    });
-  });
-
-  it("getCardColors: should fallback to specified theme colors if is not defined", () => {
-    let colors = getCardColors({
-      theme: "dark",
-    });
-    expect(colors).toStrictEqual({
-      titleColor: "#fff",
-      textColor: "#9f9f9f",
-      ringColor: "#fff",
-      iconColor: "#79ff97",
-      bgColor: "#151515",
-      borderColor: "#e4e2e2",
-    });
-  });
-
-  it("getCardColors: should return ring color equal to title color if not ring color is defined", () => {
-    let colors = getCardColors({
-      title_color: "f00",
-      text_color: "0f0",
-      icon_color: "00f",
-      bg_color: "fff",
-      border_color: "fff",
-      theme: "dark",
-    });
-    expect(colors).toStrictEqual({
-      titleColor: "#f00",
-      textColor: "#0f0",
-      iconColor: "#00f",
-      ringColor: "#f00",
-      bgColor: "#fff",
-      borderColor: "#fff",
-    });
   });
 
   it("formatBytes: should return expected values", () => {
