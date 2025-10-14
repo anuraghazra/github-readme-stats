3599105b1009b9d3da016623e88038629d4fa33f
Alexandr <qwerty541zxc@gmail.com>
2025-10-14 21:15:49 +0300

    refactor: move wrap text multiline function tests into fmt module

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -50,35 +50,3 @@ describe("Test utils.js", () => {
     ).toHaveTextContent(/Secondary Message/gim);
   });
 });
-
-describe("wrapTextMultiline", () => {
-  it("should not wrap small texts", () => {
-    {
-      let multiLineText = wrapTextMultiline("Small text should not wrap");
-      expect(multiLineText).toEqual(["Small text should not wrap"]);
-    }
-  });
-  it("should wrap large texts", () => {
-    let multiLineText = wrapTextMultiline(
-      "Hello world long long long text",
-      20,
-      3,
-    );
-    expect(multiLineText).toEqual(["Hello world long", "long long text"]);
-  });
-  it("should wrap large texts and limit max lines", () => {
-    let multiLineText = wrapTextMultiline(
-      "Hello world long long long text",
-      10,
-      2,
-    );
-    expect(multiLineText).toEqual(["Hello", "world long..."]);
-  });
-  it("should wrap chinese by punctuation", () => {
-    let multiLineText = wrapTextMultiline(
-      "专门为刚开始刷题的同学准备的算法基地，没有最细只有更细，立志用动画将晦涩难懂的算法说的通俗易懂！",
-    );
-    expect(multiLineText.length).toEqual(3);
-    expect(multiLineText[0].length).toEqual(18 * 8); // &#xxxxx; x 8
-  });
-});
