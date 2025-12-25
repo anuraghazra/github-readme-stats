78f49ccedaf3148b448a088d4f50b76f84c3d128
banu4prasad <bbanuprasad6@gmail.com>
2025-12-25 20:56:03 +0530

    add envs

--- src/common/envs.js
+++ src/common/envs.js
@@ -8,10 +8,20 @@ const gistWhitelist = process.env.GIST_WHITELIST
   ? process.env.GIST_WHITELIST.split(",")
   : undefined;
 
-const ALL_TIME_CONTRIBS=process.env.ALL_TIME_CONTRIBS === "true";
+const ALL_TIME_CONTRIBS=process.env.ALL_TIME_CONTRIBS !== "false";
+
+const ALL_TIME_CONTRIBS_TIMEOUT_MS = parseInt(
+  process.env.ALL_TIME_CONTRIBS_TIMEOUT_MS || "9000",
+  10,
+);
+
+const ALL_TIME_CONTRIBS_CONCURRENCY = parseInt(
+  process.env.ALL_TIME_CONTRIBS_CONCURRENCY || "3",
+  10,
+);
 
 const excludeRepositories = process.env.EXCLUDE_REPO
   ? process.env.EXCLUDE_REPO.split(",")
   : [];
 
-export { whitelist, gistWhitelist, excludeRepositories, ALL_TIME_CONTRIBS };
+export { whitelist, gistWhitelist, excludeRepositories, ALL_TIME_CONTRIBS, ALL_TIME_CONTRIBS_TIMEOUT_MS, ALL_TIME_CONTRIBS_CONCURRENCY };
