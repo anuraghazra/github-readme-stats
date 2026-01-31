34d72801d9cb84dfdf06bad5aa4acd8733694fb0
banu4prasad <bbanuprasad6@gmail.com>
2026-01-31 23:12:36 +0530

    remove unused imports

--- tests/fetchAllTimeContributions.test.js
+++ tests/fetchAllTimeContributions.test.js
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals";
+import { afterEach, describe, expect, it} from "@jest/globals";
 import "@testing-library/jest-dom";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
