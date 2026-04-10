1a4ed988bc047016f4855b54cebf33f02d5c40a8
Takuma IMAMURA <209989118+hyperfinitism@users.noreply.github.com>
2026-04-11 02:46:55 +0900

    fix(render): change widths based on actual Segoe UI font
    Signed-off-by: Takuma IMAMURA <209989118+hyperfinitism@users.noreply.github.com>

--- tests/renderStatsCard.test.js
+++ tests/renderStatsCard.test.js
@@ -145,7 +145,7 @@ describe("Test renderStatsCard", () => {
     });
     expect(document.querySelector("svg")).toHaveAttribute(
       "width",
-      "305.81250000000006",
+      "299.9666657447815",
     );
 
     // Test minimum card width with rank and icons.
@@ -156,7 +156,7 @@ describe("Test renderStatsCard", () => {
     });
     expect(document.querySelector("svg")).toHaveAttribute(
       "width",
-      "322.81250000000006",
+      "316.9666657447815",
     );
 
     // Test minimum card width with icons but without rank.
@@ -356,7 +356,7 @@ describe("Test renderStatsCard", () => {
 
     expect(
       document.body.getElementsByTagName("svg")[0].getAttribute("width"),
-    ).toBe("305.81250000000006");
+    ).toBe("299.9666657447815");
   });
 
   it("should auto resize if hide_rank is true & custom_title is set", () => {
